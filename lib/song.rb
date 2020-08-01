9f3b99e11c6e71e47f5b0b5fe2f997e22a3a4c25
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-01 22:30:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     song
   end
   
-  def self.create_by_filename(file_name)
+  def self.create_from_filename(file_name)
     song = self.new
     song.name = (file_name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (file_name.split(" - ")[0])

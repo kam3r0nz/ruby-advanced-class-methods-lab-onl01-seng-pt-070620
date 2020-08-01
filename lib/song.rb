2abbb50f78a4ec283e5fe6686d6e04b857ca4da0
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-01 22:28:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,5 +46,7 @@ class Song
     song = self.new
     song.name = (file_name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (file_name.split(" - ")[0])
+    @@all << song
+    song
   end
 end

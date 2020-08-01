1b551cbd3b993ffe88474726f50913d0e665440d
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-01 22:24:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,8 @@ class Song
     @@all.sort_by {|song| song.name}
   end
   
-  def self.new_from_filename(file)
-    file.split(" - ")
+  def self.new_from_filename(file_name)
+    file = self.new
+    file.name = file_name.split(" - ")
   end
 end
